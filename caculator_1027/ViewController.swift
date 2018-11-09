6ee03651e17b4b64f8f2052347e2e878ed7e753b
igysssss <1536355200@qq.com>
2018-11-09 21:01:54 +0800

    计算器+
    计算器+

--- caculator_1027/ViewController.swift
+++ caculator_1027/ViewController.swift
@@ -45,6 +45,9 @@ class ViewController: UIViewController {
     @IBAction func button0(_ sender: Any) {
         result.text = result.text!+"0"
     }
+    @IBAction func button(_ sender: Any) {
+        result.text = result.text!+"."
+    }
     @IBAction func buttonadd(_ sender: Any) {
         operatorFlag = 1
         temp = Double(result.text!)!
